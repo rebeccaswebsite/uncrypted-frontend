0d53c91dc58a1ade007d4a7b1272d0eab32760cc
github username <francisco@fcosta.pt>
2019-06-17 12:38:40 +0100

    Added app component with userData in state.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "../src/components/Navbar";
+import Search from "../src/components/Search";
+import Dashboard from "../src/components/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: {}
+    };
+  }
+
+  getUserData = () => {
+    const userURL = "http://localhost:3000/users/1";
+    return fetch(userURL).then(resp => resp.json());
+  };
+
+  componentDidMount() {
+    this.getUserData().then(resp => this.setState({ userData: resp }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
 }
-
-export default App;
