3afd095b94d4b37e39a95874538dbacd6ce61952
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-06-18 15:26:10 +0100

    Add CRUD features to Portfolio

--- src/App.js
+++ src/App.js
@@ -87,6 +87,21 @@ export default class App extends React.Component {
       .then(data => this.setState({ portfolios: data }));
   };
 
+  newPortfolio = (user, risk_profile) => {
+    const data = {
+      user_id: user.id,
+      risk_profile: risk_profile
+    }
+    return fetch('http://localhost:3000/portfolios/', {
+      method: 'POST',
+      body: data
+    })
+    .then(response =>
+      response.json().then(json => {
+        return json;
+      }))
+  }
+
   changeSelectedMarket = market => {
     const marketURL = `http://localhost:3000/markets/${market.id}`;
     return fetch(marketURL)
@@ -166,7 +181,7 @@ export default class App extends React.Component {
             path={`/my-portfolios`}
             component={props => {
               return (
-                <MyPortfolioList portfolios={this.state.userData.portfolios} />
+                <MyPortfolioList portfolios={this.state.userData.portfolios} newPortfolio={this.newPortfolio} user={this.state.userData}/>
               );
             }}
           />
--- src/components/MyPortfolioList.js
+++ src/components/MyPortfolioList.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PortfolioCard from './PortfolioCard'
 
-export default function MyPortfolioList(props) {
-    return (
-        <div>
+export default class MyPortfolioList extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             risk_profile: ''
+        }
+    }
+    
+      handleChange = event => {
+        this.setState({
+          risk_profile: event.target.value,
+        });
+      }
+
+      handleSubmit = event => {
+        event.preventDefault()
+        this.props.newPortfolio(this.props.user, this.state.risk_profile)
+      }
+     
+      render() {
+        return (
+         <div>
             <p>Click on each portfolio for more information</p>
-            {props.portfolios.map(portfolio => <PortfolioCard portfolio={portfolio}/>) }
+            {this.props.portfolios.map(portfolio => <PortfolioCard portfolio={portfolio}/>) }
+
+            <p>To add a new portfolio, enter the risk factor your portfolio will have below (0-10)</p>
+            <form onSubmit={event => this.handleSubmit(event)}>
+                <input
+                type="text"
+                risk_profile={this.state.risk_profile}
+                onChange={this.handleChange} />
+                <button>Submit</button>
+            </form>
         </div>
-    )
+        )
+      }
 }
